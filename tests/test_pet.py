3a06c231b5f0cc18b32b9838ae0b0a99c9935ad9
Юлия <loshhyonkina@icloud.com>
2026-06-06 13:55:05 +0300

    Добавила тест "Попытка удалить несуществующего питомца"

---
+++ tests/test_pet.py
@@ -0,0 +1,14 @@
+import allure
+import requests
+import unittest
+
+BASE_URL = "http://5.181.109.28:9090/api/v3/"
+
+@allure.feature("Pet")
+class TestPet(unittest.TestCase):
+    @allure.title("Попытка удалить несуществующего питомца")
+    def test_delete_nonexistent_pet(self):
+        with allure.step("Отправка запроса на удаление существующего питомца"):
+            response = requests.delete(url=f"{BASE_URL}/pet/9999")
+        with allure.step("Проверка статуса ответа"):
+            assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
