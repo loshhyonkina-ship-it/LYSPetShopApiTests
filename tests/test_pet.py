62ca0e818a3dd95187837b462f0297a0ce18e990
Юлия <loshhyonkina@icloud.com>
2026-06-06 18:52:12 +0300

    Добавила шаг "Проверка текстового содержимого ответа"

--- tests/test_pet.py
+++ tests/test_pet.py
@@ -10,5 +10,10 @@ class TestPet(unittest.TestCase):
     def test_delete_nonexistent_pet(self):
         with allure.step("Отправка запроса на удаление несуществующего питомца"):
             response = requests.delete(url=f"{BASE_URL}/pet/9999")
+
         with allure.step("Проверка статуса ответа"):
             assert response.status_code == 404, "Код ответа не совпал с ожидаемым"
+
+        with allure.step("Проверка текстового содержимого ответа"):
+            assert response.text == '{"code":404,"message":"HTTP 404 Not Found"}', \
+                "Текст ошибки не совпал с ожидаемым"
